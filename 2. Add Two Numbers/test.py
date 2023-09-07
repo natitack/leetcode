ae3db08e36797c1861ff92f26600b8d373d1e337
natitack <114112085+natitack@users.noreply.github.com>
2023-09-07 04:25:41 +0000

    add solution.py and test.py

---
+++ 2. Add Two Numbers/test.py
@@ -0,0 +1,13 @@
+import unittest
+from solution import Solution , ListNode
+
+class TestSolution(unittest.TestCase):
+        """
+    Tests generated from prompt examples
+
+    """
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
