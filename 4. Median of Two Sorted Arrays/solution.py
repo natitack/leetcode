22423cd0ee92e938e58d7b91e066f7a4c88a52b4
Nathan <nelsonna@oregonstate.edu>
2025-10-01 14:32:11 -0700

    implement solution. Fails because iterator has to be different for both lists

--- 4. Median of Two Sorted Arrays/solution.py
+++ 4. Median of Two Sorted Arrays/solution.py
@@ -2,4 +2,29 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        return 2
+        i= 0
+        count = 0
+        merged = []
+        while i < len(nums1) and i < len(nums2):
+            if nums1[i] >= nums2[i]:
+                merged.append(nums2[i])
+                merged.append(nums1[i])
+            else:
+                merged.append(nums1[i])
+                merged.append(nums2[i])
+            i += 1
+            count +=2
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+            count +=1
+        while i < len(nums2):
+            merged.append(nums2[i])
+            i += 1
+            count +=1
+        if count % 2 == 0:
+            return (merged[count // 2 - 1] + merged[count // 2]) / 2
+        else:
+            return merged[count // 2]
+
+
